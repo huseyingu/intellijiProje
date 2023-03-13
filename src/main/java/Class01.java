3dbac52090f752036d26cacc11995b09eaaa5ab1
Huseyin Guzel <hguzel6065@gmail.com>
2023-03-13 07:22:27 -0400

    v5

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,6 +2,7 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("1.calisma");
         System.out.println("v3");
+        System.out.println("bugun hava n e kadar guzel ya");
 
 
 
